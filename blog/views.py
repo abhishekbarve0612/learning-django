0d078fc164a6b925c454ef477c4b5cf319fad2b5
Abhishek Barve <barveabhishek.ab@gmail.com>
2025-01-18 18:25:19 +0530

    add comment functionality to blog posts with model, form, and templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 def post_list(request):
     post_list = Post.published.all()
@@ -40,10 +42,12 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
-        { 'post': post }
+        { 'post': post, 'comments': comments, 'form': form }
     )
     
 def post_share(request, post_id):
@@ -84,5 +88,29 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent,
+        }
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
