3db6f46f12c9eb95cbdf2afee044595524aaef61
Abhishek Barve <barveabhishek.ab@gmail.com>
2025-01-23 08:58:04 +0530

    add PostgreSQL database support and implement search functionality for blog posts

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,13 @@ from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, get_object_or_404
+from django.contrib.postgres.search import SearchVector
 from django.http import Http404
 
 from taggit.models import Tag
 
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
@@ -126,5 +127,28 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+    
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                ).filter(search=query)
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
--- my_site/settings.py
+++ my_site/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     
     'taggit',
     
@@ -82,9 +83,16 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
